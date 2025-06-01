78ab209dabd368c260350932fa12ea091dbb6c67
Bagusarya <aryabagus453@gmail.com>
2025-06-01 13:46:20 +0700

    new

--- src/component/page/tabs/portofolio.tsx
+++ src/component/page/tabs/portofolio.tsx
@@ -13,39 +13,59 @@ const data = [
     img: "/circle.png",
     title: "Circle App",
     desc: "Circle App is a modern social platform with a sleek dark UI for interactive connections. It offers smooth interactions and personalized suggestions.",
-    url: "https://circle-app-socialmedia.vercel.app"
+    url: "https://circle-app-socialmedia.vercel.app",
+    techs: [
+      "/React.svg",
+      "/chakra.png",
+      "/express-light.svg",
+      "/prisma.png",
+      "/ts.png",
+      "/postgre.png",
+    ],
   },
   {
     id: 2,
     img: "/lakoe.png",
     title: "Lakoe",
     desc: "Lakoe App is a modern e-commerce platform with a minimalist UI for easy and secure shopping. It features personalized recommendations and a smooth checkout.",
-    url: "https://www.lakoe.site/"
+    url: "https://www.lakoe.site/",
+    techs: [   "/React.svg",
+      "/chakra.png",
+      "/express-light.svg",
+      "/prisma.png",
+      "/ts.png",
+      "/postgre.png",],
   },
   {
     id: 3,
     img: "/travel.png",
     title: "TravelHunt",
     desc: "TravelHunt is a web-based decision support app that helps users choose the best travel destination in Indonesia by presenting city options with descriptions and visuals, making the decision process easier and more informed.",
-    url: "https://travel-hunt-alpha.vercel.app/"
+    url: "https://travel-hunt-alpha.vercel.app/",
+    techs: [
+      "/React.svg",
+      "/express-light.svg",
+      "/prisma.png",
+      "/ts.png",
+      "/postgre.png",
+    ],
   },
 ];
 
 export const Portofolio = () => {
-
   return (
     <div>
       <ScrollFadeIn>
-      <div className="text-[28px]">Portofolio</div>
-      <p className="text-[#969191]">
-        Several projects that I have worked on, both private and open source.
-      </p>
-      <hr className="border-b-1 border-dashed border-gray-300 mt-3 mb-10" />
-      <div className="grid  grid-cols-1 md:grid-cols-2 gap-5">
-        {data.map((item) => (
-          <Card key={item.id} item={item} />
-        ))}
-      </div>
+        <div className="text-[28px]">Portofolio</div>
+        <p className="text-[#969191]">
+          Several projects that I have worked on, both private and open source.
+        </p>
+        <hr className="border-b-1 border-dashed border-gray-300 mt-3 mb-10" />
+        <div className="grid  grid-cols-1 md:grid-cols-2 gap-5">
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
       </ScrollFadeIn>
     </div>
   );
@@ -54,13 +74,15 @@ export const Portofolio = () => {
 const Card = ({
   item,
 }: {
-  item: { img: string; title: string; desc: string; url: string };
+  item: { img: string; title: string; desc: string; url: string; techs: string[] };
 }) => {
   const [hovered, setHovered] = useState(false);
-  const {theme} = useThemeStore()
+  const { theme } = useThemeStore();
   return (
     <div
-      className={`relative w-full h-60 rounded-2xl overflow-hidden border ${theme === "dark" ? "border-[#404040]" : "border-gray-300"} `}
+      className={`relative w-full h-60 rounded-2xl overflow-hidden border ${
+        theme === "dark" ? "border-[#404040]" : "border-gray-300"
+      } `}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
@@ -82,23 +104,15 @@ const Card = ({
           >
             <div className="flex justify-between items-center">
               <h3 className="text-2xl font-semibold">{item.title}</h3>
-              <LiveDemo url={item.url}/>
+              <LiveDemo url={item.url} />
             </div>
             <p className="text-sm mt-3">{item.desc}</p>
             <div className="flex items-end h-full">
               {/* <p>Tech Stack : </p> */}
               <div className="flex gap-1">
-                <Image src={"/React.svg"} alt="" width={28} height={28} />
-                <Image src={"/chakra.png"} alt="" width={28} height={28} />
-                <Image
-                  src={"/express-light.svg"}
-                  alt=""
-                  width={28}
-                  height={28}
-                />
-                <Image src={"/prisma.png"} alt="" width={28} height={28} />
-                <Image src={"/ts.png"} alt="" width={28} height={28} />
-                <Image src={"/postgre.png"} alt="" width={28} height={28} />
+                {item.techs.map((src, idx) => (
+                  <Image key={idx} src={src} alt="" width={28} height={28} />
+                ))}
               </div>
             </div>
           </motion.div>
